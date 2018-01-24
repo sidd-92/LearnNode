f40a55dee64bfab17fc53a7b89e89f2f038a6ad0
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-24 14:10:24 -0800

    added some new lines of code

--- global.js
+++ global.js
@@ -1,1 +1,6 @@
-console.log("Global is a Object");
+var path = require("path");
+var node = "Global is a Object";
+var justNode = node.slice(12);
+console.log(`This is ${justNode}`);
+console.log(__dirname);
+console.log(__filename);
