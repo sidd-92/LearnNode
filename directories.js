5143726a3af4042edbfad42a0082eea4207c10ea
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-24 22:07:04 -0800

    I Remvoed the files from logs folder then deleted the directory

--- directories.js
+++ directories.js
@@ -1,4 +1,19 @@
 var fs = require('fs');
+//Use the anfn snippet in VS Code for async functions
+//NOTE : We cannot remove a directory which has files in it
+//First read all files in the Logs directory
 
-fs.renameSync("./assets/logs", "./logs");
-console.log("Moving Was A Sucess");
+fs.readdirSync("./logs").forEach((fileName) => {
+   //First Delete all files inside this directory using Unlink
+   
+    fs.unlinkSync("./logs/" + fileName);
+});
+
+fs.rmdir("./logs", (err) => {
+    if (err) {
+        throw err;
+    } 
+    else {
+        console.log("Everything was Okay");
+   } 
+});
