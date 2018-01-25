c42ea8272169cac71f7f427cfad6fe779b96c58c
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-24 22:39:11 -0800

    finally done with lesson 5

--- somefiles/promptPerson.js
+++ somefiles/promptPerson.js
@@ -6,25 +6,39 @@ var realPerson = {
 };
 rl.question("What is the name of person ", function (answer) {
     realPerson.name = answer;
+
+    //Use Read Stream
+    var stream = fs.createWriteStream(realPerson.name + ".md");
+    //We can write to this file using stream
+    stream.write(`${realPerson.name}\n============\n\n`);
+    
+    /*
     //the file name will be name_of_person.md
     //next arg is the contents of the file
     fs.writeFileSync(realPerson.name + ".md",`${realPerson.name}\n============\n\n`);
+    */
 
     rl.setPrompt(`What Would ${realPerson.name} say `);
     rl.prompt();
 
     //listen for any new lines
     rl.on('line', function (saying) {
-        realPerson.sayings.push(saying.trim());
-        
+               
+        /*
         //Use file system to append sayings to the md file
         fs.appendFile(realPerson.name + ".md", `*${saying.trim()}\n`);
+        */
 
         if (saying.toLowerCase().trim() === 'exit')
         {
+            stream.close();
             rl.close();
         } else
         {
+            realPerson.sayings.push(saying.trim());
+
+            //Use Stream to write the saying
+            stream.write(`*${saying.trim()}\n`);
             rl.setPrompt(`What Else would ${realPerson.name} say? ('exit' to leave) `);
             rl.prompt();
         }
