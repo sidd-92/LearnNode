75769afd7ee221f78aaf743cd81ad14e3b1f9d02
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-24 22:27:12 -0800

    Used Stream to read chunk of data

--- streams.js
+++ streams.js
@@ -1,6 +1,21 @@
 var fs = require('fs');
+var stream = fs.createReadStream("./chat.log", "UTF-8");
+var data = "";
 
-fs.readFile("./chat.log", "UTF-8", (err, chatlog) => {
-    console.log(`File Has Been Read ${chatlog.length}`);
+//To know that reading has started we can use:
+stream.once("data",() => {
+    console.log("started to read now..---------------");
+})
+
+//Listen For a Data Event
+stream.on("data", (chunk) => {
+   
+    process.stdout.write(` Chunk : ${chunk.length} |`);
+    data += chunk;
 });
-console.log("Reading Files");
+
+//Listen when it has finished reading the file
+stream.on("end", () => {
+   
+    console.log(`Finished Reading File of Size ${data.length}`);
+});
