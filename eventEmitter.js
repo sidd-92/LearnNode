70b43c7927e7bd7f71ea0e1f98682c205ac41472
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-24 17:36:19 -0800

    Added a new Person

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,9 +1,17 @@
 var Person = require('./lib/Person');
 //New Person
 var ben = new Person('Ben Franklin');
+//Another Person
+var george = new Person('George Washington');
 //Since Person is inherited it can listen for events
 
 ben.on('speak', (said) => {
     console.log(`${ben.name} : ${said}`);
 });
 ben.emit('speak', "A Circle Is Pointless");
+
+george.on('speak', (said) => {
+    console.log(`${george.name}
+    : ${said}`);
+});
+george.emit('speak', "Nice Joke Ben");
