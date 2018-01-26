1e36a235ad8063d9efd794d199a182ac75941b13
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-25 21:57:57 -0800

    Added Some lines of code to add Abraham lincoln wiki page

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,5 +1,16 @@
-describe("printName()",() => {
-    //We will write all the tests here
-    it("should print the last name first");
-    
-})
+var expect = require('chai').expect;
+var tools = require("../lib/tools");
+describe("Tools()", () => {
+    describe("printName()", () => {
+        //We will write all the tests here
+        it("should print the last name first", () => {
+            var results = tools.printName({ first: "Siddharth", last: "Natrajan" });
+            //Should/expect it to be Natrajan,Siddharth
+            expect(results).to.equal("Natrajan,Siddharth");
+        });
+    });
+
+    describe("loadWiki()", () => {
+       it("Load Abraham Lincoln Wikipedia Page") 
+    });
+});
